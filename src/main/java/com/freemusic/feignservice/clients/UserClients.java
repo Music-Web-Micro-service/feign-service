7c2e4427f1510acdf24abbfc0972c9dc90ea3506
ZHallen122 <zhallen12261@gmail.com>
2023-10-11 21:37:43 -0400

    update

--- src/main/java/com/freemusic/feignservice/clients/UserClients.java
+++ src/main/java/com/freemusic/feignservice/clients/UserClients.java
@@ -7,15 +7,6 @@ import org.springframework.web.bind.annotation.*;
 @FeignClient(value = "userservice")
 public interface UserClients {
 
-    @GetMapping("/user/search/{user_id}")
-    UserDto findById(@PathVariable("user_id") int user_id);
-
-    @GetMapping("/user/search")
-    UserDto findByUsername(@RequestParam("email") String username);
-    @GetMapping("/user/seaarch")
-
-    UserDto findByUsernaame(@RequestParam("email") String username);
-
-   // @PostMapping("/user/search")
-   // User signupArtist(@PathVariable("user_id") int user_id);
+    @GetMapping("/artists/name/{id}")
+    String getArtistNameById(@PathVariable Integer id);
 }
