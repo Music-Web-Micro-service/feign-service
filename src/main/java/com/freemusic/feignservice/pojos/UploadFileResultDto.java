5f529804d3f09ac6ec18f055df77102db50fd732
ZHallen122 <zhallen12261@gmail.com>
2023-10-11 21:31:30 -0400

    update

---
+++ src/main/java/com/freemusic/feignservice/pojos/UploadFileResultDto.java
@@ -0,0 +1,14 @@
+package com.freemusic.feignservice.pojos;
+
+import javax.print.attribute.standard.Media;
+
+public class UploadFileResultDto extends Media {
+    /**
+     * Constructs a new media attribute specified by name.
+     *
+     * @param value a value
+     */
+    protected UploadFileResultDto(int value) {
+        super(value);
+    }
+}
